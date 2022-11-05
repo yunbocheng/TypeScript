f5213bafe676a1c6640d0b0498dce6c9c7de38f4
程云博 <chengyunbo@gyyx.cn>
2022-11-05 18:28:33 +0800

    feat: 初始化后端框架

---
+++ src/main/java/com/yunbocheng/typescript/TypeScriptApplication.java
@@ -0,0 +1,13 @@
+package com.yunbocheng.typescript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeScriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunbocheng/typescript/TypeScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunbocheng.typescript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
